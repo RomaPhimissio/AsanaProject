85d3eb19c413ba9d5215845204cc487b712cf995
Your Name <you@example.com>
2023-07-29 13:37:34 +0300

    fix fix

--- docs/js/script.js
+++ docs/js/script.js
@@ -14,6 +14,7 @@ window.addEventListener("load", () => {
    }
 
    // ! Burger
+
    if (burger) {
       body.addEventListener("click", burgerToggle);
       function burgerToggle(e) {
@@ -21,17 +22,17 @@ window.addEventListener("load", () => {
             if (burger.classList.contains("active")) {
                burger.classList.remove("active");
                header.classList.remove("active");
-               body.classList.remove("lock");
+               body.classList.remove("lock-scroll");
             } else {
                burger.classList.add("active");
                header.classList.add("active");
-               body.classList.add("lock");
+               body.classList.add("lock-scroll");
                window.addEventListener("scroll", closeBurger); // Закрывает бургер при скролле в том случае, когда для Body не задан класс 'lock'
             }
          } else if (!e.target.closest(".burger") && !e.target.closest(".menu")) {
             burger.classList.remove("active");
             header.classList.remove("active");
-            body.classList.remove("lock");
+            body.classList.remove("lock-scroll");
             window.removeEventListener("scroll", closeBurger);
          }
       }
@@ -41,11 +42,24 @@ window.addEventListener("load", () => {
             burger.classList.remove("active");
             menu.classList.remove("active");
             header.classList.remove("active");
-            body.classList.remove("lock");
+            body.classList.remove("lock-scroll");
             window.removeEventListener("scroll", closeBurger);
          }
       }
    }
+
+   // ! Catalog
+
+   const headerLinkOpen = document.querySelector('.header__link-open');
+   const catalogMainLink = document.querySelector('.catalog__main-link');
+
+   headerLinkOpen.addEventListener('click', function () {
+      header.classList.add('current');
+   });
+
+   catalogMainLink.addEventListener('click', function () {
+      header.classList.remove('current');
+   });
 
 
    // ! Dropdown 
@@ -129,7 +143,7 @@ window.addEventListener("load", () => {
 
 // ! Swiper
 
-new Swiper('.swiper', {
+new Swiper('.hero__block-picture', {
    // клавиатура
    keyboard: {
       enabled: true,
@@ -138,7 +152,7 @@ new Swiper('.swiper', {
    },
    speed: 600,
    direction: "vertical",
-   mousewheel: true,
+   // mousewheel: true,
    pagination: {
       el: ".hero__pagination",
       clickable: true,
@@ -156,15 +170,193 @@ new Swiper('.swiper', {
    spaceBetween: 30,
 });
 
+new Swiper('.features', {
+   // клавиатура
+   keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true
+   },
+
+   breakpoints: {
+      0: {
+         slidesPerView: 1.1,
+         spaceBetween: 15
+
+      },
+      800: {
+         slidesPerView: 2.1,
+         spaceBetween: 30
+      },
+      1200: {
+         slidesPerView: 3,
+         spaceBetween: 40
+      }
+   }
+})
+
 // ! Rating stars 
 
-// const stars = document.querySelectorAll('.star');
+const starContainers = document.querySelectorAll('.stars');
 
-// stars.forEach((star, index1) => {
-//    star.addEventListener("click", () => {
-//       stars.forEach((star, index2) => {
-//          index1 >= index2 ? star.classList.add('active') : star.classList.remove('active');
-//       })
-//    })
-// })
+starContainers.forEach(starContainer => {
+   const stars = starContainer.querySelectorAll('.stars__star');
 
+   stars.forEach((star, index1) => {
+      star.addEventListener("click", () => {
+         stars.forEach((star, index2) => {
+            index1 >= index2 ? star.classList.add('active') : star.classList.remove('active');
+         });
+      });
+   });
+});
+
+
+// ! Отмена скролла при клике на ссылку
+
+const allLinks = document.querySelectorAll('a');
+
+allLinks.forEach((link) => {
+   link.addEventListener('click', (event) => {
+      event.preventDefault(); // Отменяем действие по умолчанию (скроллинг вверх)
+
+      // Дополнительный код для всех ссылок
+      // ...
+   });
+});
+
+
+// ! pop-up 
+
+// Получаем ссылки на элементы кнопки и попапа
+const buttonUser = document.querySelector('.header__bar-btn-user');
+const popUp = document.querySelector('.pop-up');
+const btnClose = document.querySelector('.pop-up__btn-close');
+
+// Добавляем обработчик события клика на кнопку "Открыть попап"
+buttonUser.addEventListener('click', () => {
+   // При каждом клике переключаем класс "active" у попапа
+   popUp.classList.toggle('active');
+
+   // Добавляем или удаляем класс "lock-scroll" у body в зависимости от состояния попапа
+   document.body.classList.toggle('lock-scroll', popUp.classList.contains('active'));
+});
+
+// Добавляем обработчик события клика на кнопку "Закрыть попап"
+btnClose.addEventListener('click', () => {
+   // Убираем класс "active" у попапа и удаляем класс "lock-scroll" у body
+   popUp.classList.remove('active');
+   document.body.classList.remove('lock-scroll');
+});
+
+// ! pop-up-log-in
+
+// Получаем ссылки на элементы
+const btnPopUp = document.querySelector('.btn-pop-up');
+const popUpLog = document.querySelector('.pop-up-log');
+const btnCloseLog = document.querySelector('.pop-up-log__btn-close');
+
+// Добавляем обработчик события клика на кнопку
+btnPopUp.addEventListener('click', () => {
+   // При клике добавляем класс "active" к попапу pop-up-log
+   popUpLog.classList.add('active');
+
+   // Убираем класс "active" у другого попапа pop-up
+   popUp.classList.remove('active');
+});
+
+// Добавляем обработчик события клика на кнопку "Закрыть" в pop-up-log
+btnCloseLog.addEventListener('click', () => {
+   // Убираем класс "active" у попапа pop-up-log при клике на кнопку "Закрыть"
+   popUpLog.classList.remove('active');
+});
+
+// ! pop-up-password
+
+const linkPassword = document.querySelector('.pop-up-log__password-link');
+const popUpPassword = document.querySelector('.pop-up-password');
+
+linkPassword.addEventListener('click', () => {
+   popUpLog.classList.remove('active');
+   popUpPassword.classList.add('active');
+});
+
+const btnClosePassword = document.querySelector('.pop-up-password__btn-close');
+
+btnClosePassword.addEventListener('click', () => {
+   popUpPassword.classList.remove('active'); // Убираем класс "active" у pop-up-password при закрытии
+});
+
+
+// ! breadcrumbs Btn
+
+// Получаем все элементы "breadcrumb" для каждого блока
+const productsBreadcrumbs = document.querySelectorAll('.products__breadcrumbs .breadcrumb');
+const trendBreadcrumbs = document.querySelectorAll('.trend__breadcrumbs .breadcrumb');
+
+// Добавляем обработчик события "click" для каждого элемента в блоке "products__breadcrumbs"
+productsBreadcrumbs.forEach(breadcrumb => {
+   breadcrumb.addEventListener('click', () => {
+      // Удаляем класс "active" у всех элементов "breadcrumb" внутри текущего блока
+      productsBreadcrumbs.forEach(crumb => {
+         crumb.classList.remove('active');
+      });
+
+      // Добавляем класс "active" только для текущего элемента
+      breadcrumb.classList.add('active');
+   });
+});
+
+// Добавляем класс "active" только первому элементу в блоке "products__breadcrumbs"
+productsBreadcrumbs[0].classList.add('active');
+
+// Добавляем обработчик события "click" для каждого элемента в блоке "trend__breadcrumbs"
+trendBreadcrumbs.forEach(breadcrumb => {
+   breadcrumb.addEventListener('click', () => {
+      // Удаляем класс "active" у всех элементов "breadcrumb" внутри текущего блока
+      trendBreadcrumbs.forEach(crumb => {
+         crumb.classList.remove('active');
+      });
+
+      // Добавляем класс "active" только для текущего элемента
+      breadcrumb.classList.add('active');
+   });
+});
+
+// Добавляем класс "active" только первому элементу в блоке "trend__breadcrumbs"
+trendBreadcrumbs[0].classList.add('active');
+
+
+
+// ! breadcrumbs sizes
+
+// Получаем все элементы "card__sizes"
+const cardSizes = document.querySelectorAll('.card__sizes');
+
+// Добавляем класс "active" к второй кнопке в каждом блоке
+cardSizes.forEach(sizes => {
+   const buttons = sizes.querySelectorAll('.card__btn');
+   buttons[1].classList.add('active');
+});
+
+// Получаем все элементы "card__btn"
+const breadcrumbsBtn = document.querySelectorAll('.card__btn');
+
+// Добавляем обработчик события "click" для каждого элемента
+breadcrumbsBtn.forEach(breadcrumb => {
+   breadcrumb.addEventListener('click', () => {
+      // Получаем родительский элемент текущей кнопки
+      const parentSizes = breadcrumb.closest('.card__sizes');
+
+      // Получаем все элементы "card__btn" внутри родительского элемента
+      const btnsInParent = parentSizes.querySelectorAll('.card__btn');
+
+      // Удаляем класс "active" у всех элементов "card__btn" внутри родительского элемента
+      btnsInParent.forEach(btn => {
+         btn.classList.remove('active');
+      });
+
+      // Добавляем класс "active" только для текущей кнопки
+      breadcrumb.classList.add('active');
+   });
+});
